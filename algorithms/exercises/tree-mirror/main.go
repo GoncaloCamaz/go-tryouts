77ecf94e28ab3a513004499d2263e269df944322
GoncaloCamaz <gcamaz@sapo.pt>
2024-09-24 21:01:09 +0100

    feat: add more tree exercises

--- algorithms/exercises/tree-mirror/main.go
+++ algorithms/exercises/tree-mirror/main.go
@@ -1,3 +1,66 @@
 package main
 
-// todo
+import "fmt"
+
+type TreeNode struct {
+	Value int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func printInOrder(tree *TreeNode) {
+	if tree == nil {
+		return
+	}
+
+	printInOrder(tree.Left)
+	fmt.Printf("%d ", tree.Value)
+	printInOrder(tree.Right)
+}
+
+func mirror(tree *TreeNode) *TreeNode {
+	if tree == nil {
+		return nil
+	}
+
+	left := mirror(tree.Left)
+	right := mirror(tree.Right)
+
+	tree.Left = right
+	tree.Right = left
+
+	return tree
+}
+
+func main() {
+	tree := &TreeNode{
+		Value: 1,
+		Left: &TreeNode{
+			Value: 2,
+			Left: &TreeNode{
+				Value: 4,
+			},
+			Right: &TreeNode{
+				Value: 5,
+			},
+		},
+		Right: &TreeNode{
+			Value: 3,
+			Left: &TreeNode{
+				Value: 6,
+			},
+			Right: &TreeNode{
+				Value: 7,
+			},
+		},
+	}
+
+	fmt.Printf("Original tree: %+v\n", tree)
+	printInOrder(tree)
+	fmt.Println()
+
+	mirrorTree := mirror(tree)
+	fmt.Printf("Mirrored tree: %+v\n", mirrorTree)
+	printInOrder(mirrorTree)
+	fmt.Println()
+}
